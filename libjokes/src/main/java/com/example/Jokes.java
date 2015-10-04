82f25df06c86ba13e2f1f23acd285296cb95f221
Andy <thingswithhandles@gmail.com>
2015-10-04 17:31:19 -0500

    Step 1 complete

---
+++ libjokes/src/main/java/com/example/Jokes.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Jokes {
+    public String tellJoke() {
+        return "I bought the world's worst thesaurus yesterday. " +
+                "Not only is it terrible, it's terrible.";
+    }
+}
